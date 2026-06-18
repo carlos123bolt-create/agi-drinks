ebd3501bdb003ef7e4d150850183c89f0509e042
carlos123bolt-create <carlos123bolt@gmail.com>
2026-06-18 20:43:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -272,8 +272,8 @@ function enviarPedidoWhatsApp() {
     textoMsg += `*Subtotal:* R$ ${subtotal.toFixed(2).replace('.',',')}\n`;
     
     if(tipoEntrega === 'entrega') {
-        const rua = document.getElementById('end-rua').value;
-        const comp = document.getElementById('end-complemento').value;
+        const rua = document.getElementById('rua').value;
+        const comp = document.getElementById('complemento').value;
         const regiao = frete === 5 ? "Diadema" : "Outra Região";
 
         if(!rua) {
