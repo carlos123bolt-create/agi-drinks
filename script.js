e9ecaa2e4f95c4371d5f9723927ab05f332d792a
carlos123bolt-create <carlos123bolt@gmail.com>
2026-06-18 22:03:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -282,7 +282,6 @@ function enviarPedidoWhatsApp() {
         textoMsg += `*Frete (${regiao}):* R$ ${frete.toFixed(2).replace('.',',')}\n`;
         textoMsg += `*Total:* R$ ${totalGeral.toFixed(2).replace('.',',')}\n\n`;
         textoMsg += `*Modo:* 🚀 Entrega em Casa\n`;
-        textoMsg += `*Nome:* {cliente-nome}\n ;
         textoMsg += `*Endereço:* ${rua}\n`;
     } else {
         textoMsg += `*Total:* R$ ${totalGeral.toFixed(2).replace('.',',')}\n\n`;
